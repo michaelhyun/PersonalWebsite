2f638fbcccaad95e6239793d33b9f34ca0ab50cb
Michael Hyun <michael.hyun3@gmail.com>
2022-07-06 00:43:25 -0700

    Add scss files for starter components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Introduction from "./components/introduction/Introduction";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header/>
+      <Introduction/>
+      <Footer/>
     </div>
   );
 }
